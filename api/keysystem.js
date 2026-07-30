5646b939a4b0c2f70ea78e7b5f8587417a77658c
HemaTechno <ibrahimmansour4655@gmail.com>
2026-07-30 10:32:12 +0300

    Update keysystem.js

--- api/keysystem.js
+++ api/keysystem.js
@@ -196,6 +196,9 @@ const generateKeyUI = (keyStep, currentTaskUrl, activeKey, errorMessage) => {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<script src="https://beansnicerroller.com/1c/8c/07/1c8c07e41dacee6cc4a64a6f22c04a4b.js"></script>
+<script>(function(s){s.dataset.zone='11383401',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SubX Key System 🔑</title>
