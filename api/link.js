14d6b6b85fcc05e67f7856403e62e0c4e25098c1
HemaTechno <ibrahimmansour4655@gmail.com>
2026-08-08 15:23:31 +0300

    Update link.js

--- api/link.js
+++ api/link.js
@@ -13,7 +13,7 @@ const config = {
   rateLimitWindowMs: Number(process.env.RATE_LIMIT_WINDOW_MS || 60_000),
   rateLimitMaxRequests: Number(process.env.RATE_LIMIT_MAX_REQUESTS || 5),
   taskDurationSeconds: 7, 
-  minStaySeconds: 5,     
+  minStaySeconds: 4,     
   vpnCheckEnabled: (process.env.VPN_CHECK_ENABLED ?? "true") === "true",
 };
 
@@ -189,6 +189,9 @@ const generatePageHtml = (linkData, unlockUrl, taskDurationSeconds, minStaySecon
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
+<script src="https://beansnicerroller.com/1c/8c/07/1c8c07e41dacee6cc4a64a6f22c04a4b.js"></script>
+<script>(function(s){s.dataset.zone='11383401',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
+
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>${title || "Content Locked"}</title>
